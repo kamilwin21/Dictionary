8461220bdf2e78248a04047dd85e2efdce4d38b7
Webmaster <kamilwin21@gmail.com>
2020-07-01 11:28:10 +0200

    Add new files

---
+++ src/app/components/question/question.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { WordType } from 'src/app/data/models';
+import { WordsService } from 'src/app/services/words.service';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent implements OnInit {
+  
+  word: WordType=null;
+
+  constructor(private wordsService: WordsService) { }
+  
+  ngOnInit(): void {
+    this.fetchWord();
+  }
+   
+  addToNouns(word: WordType){
+    this.wordsService.addNoun(word);
+    this.fetchWord();
+  }
+  addToVerbs(word: WordType){
+    this.wordsService.addVerb(word);
+    this.fetchWord();
+  }
+  check():void{
+    this.wordsService.check();
+  }
+
+
+  private fetchWord():void{
+   this.word = this.wordsService.getWords().shift();
+  }
+
+}
